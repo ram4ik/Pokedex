7ea490b9b517cab99fcecd56b5afd898c5362b90
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-22 09:46:56 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by ramil on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
